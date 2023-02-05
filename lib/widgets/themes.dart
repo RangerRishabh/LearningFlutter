2778b8eb0ed8e181f5fb983f052e794b6063774d
Rishabh Jha <rishabhjha018@gmail.com>
2023-02-05 13:47:05 +0530

    day 12 completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -5,11 +5,12 @@ class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
       fontFamily: GoogleFonts.lato().fontFamily,
-      appBarTheme: AppBarTheme(
+      appBarTheme: const AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
         iconTheme: IconThemeData(color: Colors.black),
-        textTheme: Theme.of(context).textTheme,
+        titleTextStyle: TextStyle(color: Colors.black),
+        
       ));
 
 static ThemeData darkTheme(BuildContext context) => ThemeData(
